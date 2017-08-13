5fde1d76f2e891d85cf80c15971f649c17d131e1
Robert Hood <26657222+RHood78@users.noreply.github.com>
2017-08-12 23:13:22 -0600

    Update change_keys.sh

--- change_keys.sh
+++ change_keys.sh
@@ -8,6 +8,6 @@ if [ "$first_boot" = 'yes' ]; then
 	sudo ssh-keygen -t rsa1 -f /etc/ssh/ssh_host_rsa_key
 	sudo ssh-keygen -t dsa  -f /etc/ssh/ssh_host_dsa_key
         sudo rm /home/bob/monerodo/conf_files/firstboot
-        echo "no" > /home/$USER/monerodo/conf_files/firstboot
-        sudo cp /home/$USER/monerodo/conf_files/first_time.txt /monerodo/
+        echo "no" > /home/bob/monerodo/conf_files/firstboot
+        sudo cp /home/bob/monerodo/conf_files/first_time.txt /monerodo/
 fi

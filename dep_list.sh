bd5bf969e7ce6eb0482994fed9f5c63c948c2155
Robert Hood <26657222+RHood78@users.noreply.github.com>
2017-08-12 19:25:16 -0600

    Update dep_list.sh

--- dep_list.sh
+++ dep_list.sh
@@ -4,4 +4,3 @@
 sudo apt-get install -y git gcc-4.9 cmake libunbound2 libevent-2.0-5 libgtest-dev \
 libboost1.55-all-dev libevent-dev libssl-dev build-essential pkg-config \
 miniupnpc libdb++-dev libdb-dev
-

9a14e6e573c5cb34cb2837c476b50f86ad69e19c
Robert Hood <26657222+RHood78@users.noreply.github.com>
2017-08-12 21:00:17 -0600

    Update wait4sync.sh
    bob to $USER

--- conf_files/wait4sync.sh
+++ conf_files/wait4sync.sh
@@ -7,15 +7,14 @@ do
 
 if [ "$(grep "start monero-wallet-cli"  /.bitmonero/bitmonero.log)" ] ;
 then
-echo "Monero Core is synchronized!" > /home/bob/.monerodo/status.txt
+echo "Monero Core is synchronized!" > /home/$USER/.monerodo/status.txt
 initctl emit bitmonero_sync
 exit
 
 else
-echo "Monero Core is still synchronizing. Nothing will work." > /home/bob/.monerodo/status.txt
+echo "Monero Core is still synchronizing. Nothing will work." > /home/$USER/.monerodo/status.txt
 fi
 
 sleep 3
 
 done
-
